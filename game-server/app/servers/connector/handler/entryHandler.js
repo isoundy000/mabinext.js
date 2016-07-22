75bb2c866a71d0fafb8c7b3fe89e662e72e0b6b1
Colin Trinity <colintrinityls@gmail.com>
2016-07-22 20:00:43 +0900

    init project

---
+++ game-server/app/servers/connector/handler/entryHandler.js
@@ -0,0 +1,61 @@
+var Code = require('../../../../../shared/code');
+
+module.exports = function(app) {
+  return new Handler(app);
+};
+
+var Handler = function(app) {
+  this.app = app;
+};
+
+/**
+ * New client entry.
+ *
+ * @param  {Object}   msg     request message
+ * @param  {Object}   session current session object
+ * @param  {Function} next    next step callback
+ * @return {Void}
+ */
+Handler.prototype.entry = function(msg, session, next) {
+    var code = Code.OK;
+    var message = "login ok";
+    
+    if(msg.username === "colin" || msg.username === "test") {
+        code = Code.FAIL;
+        message = "login failed";
+    }
+    
+    next(null, {code: code, msg: message});
+};
+
+/**
+ * Publish route for mqtt connector.
+ *
+ * @param  {Object}   msg     request message
+ * @param  {Object}   session current session object
+ * @param  {Function} next    next step callback
+ * @return {Void}
+ */
+Handler.prototype.publish = function(msg, session, next) {
+	var result = {
+		topic: 'publish',
+		payload: JSON.stringify({code: 200, msg: 'publish message is ok.'})
+	};
+  next(null, result);
+};
+
+/**
+ * Subscribe route for mqtt connector.
+ *
+ * @param  {Object}   msg     request message
+ * @param  {Object}   session current session object
+ * @param  {Function} next    next step callback
+ * @return {Void}
+ */
+Handler.prototype.subscribe = function(msg, session, next) {
+	var result = {
+		topic: 'subscribe',
+		payload: JSON.stringify({code: 200, msg: 'subscribe message is ok.'})
+	};
+  next(null, result);
+};
